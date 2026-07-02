fbb917468c4c456a75d50e5b12aee1ec11868c2b
aryasatyadev047 <aryasatyadev047@gmail.com>
2026-07-02 16:24:31 +0530

    Delete backend directory

--- backend/sockets/socketHandler.js
+++
@@ -1,9 +0,0 @@
-module.exports = (io) => {
-  io.on("connection", (socket) => {
-    console.log(`✅ User Connected: ${socket.id}`);
-
-    socket.on("disconnect", () => {
-      console.log(`❌ User Disconnected: ${socket.id}`);
-    });
-  });
-};
